38bad6e84fda6e347bc16066deec7024923844ea
FilipeSS01 <filipess671@gmail.com>
2022-11-12 23:23:48 -0300

    Primeiro programa em Java

---
+++ src/house/Doors.java
@@ -0,0 +1,51 @@
+public class Doors {
+    private String cor;
+    private Boolean aberta;
+    private Double dimensaoX;
+    private Double dimensaoY;
+    private Double dimensaoZ;
+
+    // Gets
+    public String getCor(){
+        return this.cor;
+    }
+    public Boolean getAberta(){
+        return this.aberta;
+    }
+    public Double getDimensaoX(){
+        return this.dimensaoX;
+    }
+    public Double getDimensaoY(){
+        return this.dimensaoY;
+    }
+    public Double getDimensaoZ(){
+        return this.dimensaoZ;
+    }
+    // Sets
+    public void setCor(String value){
+        this.cor = value;
+    }
+    public void setAberta(Boolean value){
+        this.aberta = value;
+    }    
+    public void setDimensaoX(Double value){
+        this.dimensaoX = value;
+    }
+    public void setDimensaoY(Double value){
+        this.dimensaoY = value;
+    }
+    public void setDimensaoZ(Double value){
+        this.dimensaoZ = value;
+    }
+
+    // Metodos
+    public void toggleDoors(){
+        this.setAberta(!this.aberta);
+    }
+    public void paintDoors(String value){
+        this.setCor(value);
+    }
+    public Boolean statusDoors() {
+        return this.getAberta();
+    }
+}
---
+++ src/house/Main.java
@@ -0,0 +1,40 @@
+public class Main{
+    public static void main(String [] args){
+        // Programa 1
+        Doors door = new Doors();
+        door.setAberta(true);
+        door.setCor("Vermelha");
+        door.setDimensaoX(10.0);
+        door.setDimensaoY(10.0);
+        door.setDimensaoZ(10.0);
+        System.out.println("===================== Projeto 1 =========================");
+        System.out.println("Cor: " + door.getCor());
+        System.out.println(door.statusDoors() ? "Porta: Aberta" : "Porta: Fechada");
+        System.out.println("Dimensão X: " + door.getDimensaoX());
+        System.out.println("Dimensão Y: " + door.getDimensaoY());
+        System.out.println("Dimensão Z: " + door.getDimensaoZ());
+
+        door.paintDoors("Amarelo");
+        door.toggleDoors();
+        System.out.println("\nCor: " + door.getCor());
+        System.out.println(door.statusDoors() ? "Porta: Aberta" : "Porta: Fechada");
+        System.out.println("Dimensão X: " + door.getDimensaoX());
+        System.out.println("Dimensão Y: " + door.getDimensaoY());
+        System.out.println("Dimensão Z: " + door.getDimensaoZ());
+        System.out.println("=========================================================\n");
+
+        // Programa 2
+        House house = new House();
+        house.setCor("Rosa");
+        house.setDoor1(true);
+        house.setDoor2(false);
+        house.setDoor3(false);
+
+        house.paintHouse("Verde Neon");
+        System.out.println("===================== Projeto 2 =========================");
+        System.out.println("Cor: " + house.getCor());
+        System.out.println("Portas abertas: " + house.doorsOpen());
+        System.out.println("=========================================================");
+
+    }
+}
