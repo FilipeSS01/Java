71910af17f2e3fdee05b7ea558e39ac268e84ba0
FilipeSS01 <filipess671@gmail.com>
2022-11-16 17:44:24 -0300

    Banco: Conta de investimento

--- src/Banco/Investimento.java
+++ src/Banco/Investimento.java
@@ -1,5 +1,25 @@
 package Banco;
 
 public class Investimento extends Conta{
-    
+    public Investimento(String primeiroNome, String ultimoNome, int cpf, int conta, double saldo) {
+        super(primeiroNome, ultimoNome, cpf, conta, saldo);
+    }
+
+    public String mes() {
+        super.saldo = (super.saldo * 1.013) - 10;
+        return super.saldo() + "\nRendimento: R$ " + df.format(super.saldo - (super.saldo / 1.013)) + "\nManutenção: R$ " + df.format(10);
+    }
+
+    public String saque(double value) {
+        super.saldo = super.saldo - 25;
+        return super.saque(value) + "\nValor da operação: R$ " + df.format(25);
+    }
+
+    public String deposito(double value) {
+        return super.deposito(value);
+    }
+
+    public String saldo() {
+        return super.saldo();
+    }
 }
--- src/MainBanco.java
+++ src/MainBanco.java
@@ -1,10 +1,12 @@
 
 import Banco.Corrente;
+import Banco.Investimento;
 import Banco.Poupanca;
 
 public class MainBanco {
     public static void main(String[] args) {
         Poupanca contaPoupanca = new Poupanca("Filipe", "Silva", 1321790863, 34546546, 1000000);
+        Investimento contaInvestimento = new Investimento("Filipe", "Silva", 1321790863, 34546546, 1000000);
         Corrente contaCorrente = new Corrente("Tyler", "Silva", 1321790863, 45646465, 5);
         System.out.println(contaPoupanca.saldo() + "\n");
         System.out.println(contaPoupanca.mes() + "\n");
@@ -15,5 +17,11 @@ public class MainBanco {
         System.out.println(contaCorrente.mes() + "\n");
         System.out.println(contaCorrente.mes() + "\n");
         System.out.println(contaCorrente.mes() + "\n");
+        System.out.println("================================================\n");
+        System.out.println(contaInvestimento.saldo() + "\n");
+        System.out.println(contaInvestimento.mes() + "\n");
+        System.out.println(contaInvestimento.mes() + "\n");
+        System.out.println(contaInvestimento.mes() + "\n");
+        System.out.println(contaInvestimento.saque(1000000) + "\n");
     }
 }
